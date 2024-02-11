38955310f6e53218d76e95fd8f7c6e1e78679ef1
osnick50 <oskar.nicz@gmail.com>
2024-02-11 13:22:14 +0100

    feat: add logging in database module

--- database.py
+++ database.py
@@ -10,15 +10,19 @@ ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
 connector = Connector(ip_type)
 
 def getconn() -> pymysql.connections.Connection:
-    logging.info("Initialize connection with DB")
-    conn: pymysql.connections.Connection = connector.connect(
-        os.getenv('INSTANCE_CONNECTION_NAME'),
-        'pymysql',
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASS'),
-        db=os.getenv('DB_NAME'),
-    )
-    return conn
+    try:
+        logging.info("Initialize connection with DB")
+        conn: pymysql.connections.Connection = connector.connect(
+            os.getenv('INSTANCE_CONNECTION_NAME'),
+            'pymysql',
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASS'),
+            db=os.getenv('DB_NAME'),
+        )
+        return conn
+    except Exception as e:
+        logging.error("Error initializing DB connection: ", e)
+
 
 engine = sqlalchemy.create_engine(
     "mysql+pymysql://",
