fe2693c098599a649da7b3f1f536e8b7114bd33b
osnick50 <oskar.nicz@gmail.com>
2024-02-17 22:37:33 +0100

    feat: add GCP SQL DB connection, dynamic view for each job

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import logging
 from flask import Flask, render_template, jsonify
-from database import load_jobs_db
+from database import load_jobs_db, load_job_db
 
 # template_dir = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'frontend/templates')
 template_dir = 'frontend/templates'
 app = Flask(__name__, template_folder=template_dir)
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route("/")
@@ -14,9 +14,20 @@ def home_page():
     try:
         jobs = load_jobs_db()
         logging.info("Jobs info loaded on page")
-        return render_template('home.html', jobs=jobs, company_name="Best Jobs")
+        return render_template('home.html', jobs=jobs)
     except Exception as e:
-        logging.info("Error while jobs loading: ", e)
+        logging.error("Error while jobs loading: ", e)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+     try:
+        job = load_job_db(id)
+        if not job:
+            return "Not found", 404
+        return render_template('jobpage.html', job=job)
+     except Exception as e:
+        logging.error("Error while jobs loading: ", e) 
 
 
 @app.route("/api/jobs")
@@ -26,7 +37,8 @@ def list_jobs():
         logging.info("Jobs info loaded")
         return jsonify(jobs)
     except Exception as e:
-        logging.info("Error while jobs loading: ", e) 
+        logging.error("Error while jobs loading: ", e) 
+
 
 @app.route("/api/health")
 def health_check():
--- database.py
+++ database.py
@@ -7,7 +7,12 @@ logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
-db_connection = mysql.connector.connect(user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), database=os.getenv("DB_NAME"))
+db_connection = mysql.connector.connect(
+    user=os.getenv("DB_USER"), 
+    password=os.getenv("DB_PASSWORD"), 
+    host=os.getenv("DB_HOST"), 
+    database=os.getenv("DB_NAME")
+    )
 
 def load_jobs_db():
     try:
@@ -20,4 +25,18 @@ def load_jobs_db():
         logging.info("Jobs loaded from DB")
         return jobs
     except Exception as e:
-        logging.info("Error occured while loading jobs from DB: ", e)
+        logging.error("Error occured while loading jobs from DB: ", e)
+
+def load_job_db(id):
+    try:
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute((f"SELECT * FROM jobs WHERE id = {id}"))
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return None
+        else:
+            return result[0]
+    except Exception as e:
+        logging.error("Error occurred while getting job by ID:", e)
+
+
