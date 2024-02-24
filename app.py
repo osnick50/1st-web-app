04eb89053a53ba59252b5f67d448def49e5b31d4
osnick50 <oskar.nicz@gmail.com>
2024-02-24 21:08:56 +0100

    feat: add application to DB, add alert for application submit success/error

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 import logging
-from flask import Flask, render_template, jsonify
-from database import load_jobs_db, load_job_db
+from flask import Flask, render_template, jsonify, request, flash, redirect, url_for
+from database import load_jobs_from_db, load_job_id, add_application_to_db
 
-# template_dir = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),'frontend/templates')
-template_dir = 'frontend/templates'
-app = Flask(__name__, template_folder=template_dir)
+TEMPLATE_DIR = 'frontend/templates'
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")  # Set a secret key for flashing messages, replace with your own secret key
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route("/")
 def home_page():
     try:
-        jobs = load_jobs_db()
+        jobs = load_jobs_from_db()
         logging.info("Jobs info loaded on page")
         return render_template('home.html', jobs=jobs)
     except Exception as e:
@@ -21,19 +22,35 @@ def home_page():
 
 @app.route("/job/<id>")
 def show_job(id):
-     try:
-        job = load_job_db(id)
+    try:
+        job = load_job_id(id)
         if not job:
             return "Not found", 404
         return render_template('jobpage.html', job=job)
-     except Exception as e:
+    except Exception as e:
         logging.error("Error while jobs loading: ", e) 
 
+
+@app.route("/job/<id>/apply",  methods=["post"])
+def apply_to_job(id):
+    try:
+        data = request.form
+        response = add_application_to_db(id, data)
+        if response:
+            flash('Application submitted successfully!', 'success')
+            return redirect(url_for('show_job', id=id))  # Redirect to /job/1 instead of rendering jobpage.html
+        else:
+            flash('Error occurred during job application', 'error')
+            return redirect(url_for('show_job', id=id))
+    except Exception as e:
+        logging.error(f"Apply error: {e}")
+        flash('Error occurred during job application', 'error')
+        return redirect(url_for('show_job', id=id))
 
 @app.route("/api/jobs")
 def list_jobs():
     try:
-        jobs = load_jobs_db()
+        jobs = load_jobs_from_db()
         logging.info("Jobs info loaded")
         return jsonify(jobs)
     except Exception as e:
@@ -43,6 +60,7 @@ def list_jobs():
 @app.route("/api/health")
 def health_check():
         return jsonify({'status': 'ok'})
-    
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -7,17 +7,20 @@ logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
-db_connection = mysql.connector.connect(
-    user=os.getenv("DB_USER"), 
-    password=os.getenv("DB_PASSWORD"), 
-    host=os.getenv("DB_HOST"), 
-    database=os.getenv("DB_NAME")
-    )
+def connect_with_db():
+    db_connection = mysql.connector.connect(
+        user=os.getenv("DB_USER"), 
+        password=os.getenv("DB_PASSWORD"), 
+        host=os.getenv("DB_HOST"), 
+        database=os.getenv("DB_NAME")
+        )
+    return db_connection
 
-def load_jobs_db():
+def load_jobs_from_db():
     try:
-        cursor = db_connection.cursor(dictionary=True)
-        cursor.execute(("SELECT * FROM jobs"))
+        db_conn = connect_with_db()
+        cursor = db_conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM jobs")
         result = cursor.fetchall()
         jobs = []
         for dict_row in result:
@@ -26,11 +29,17 @@ def load_jobs_db():
         return jobs
     except Exception as e:
         logging.error("Error occured while loading jobs from DB: ", e)
+    finally:
+        if db_conn.is_connected():
+            cursor.close()
+            db_conn.close()
+            logging.info("Database connection closed")
 
-def load_job_db(id):
+def load_job_id(id):
     try:
-        cursor = db_connection.cursor(dictionary=True)
-        cursor.execute((f"SELECT * FROM jobs WHERE id = {id}"))
+        db_conn = connect_with_db()
+        cursor = db_conn.cursor(dictionary=True)
+        cursor.execute(f"SELECT * FROM jobs WHERE id = {id}")
         result = cursor.fetchall()
         if len(result) == 0:
             return None
@@ -38,5 +47,41 @@ def load_job_db(id):
             return result[0]
     except Exception as e:
         logging.error("Error occurred while getting job by ID:", e)
+    finally:
+        if db_conn.is_connected():
+            cursor.close()
+            db_conn.close()
+            logging.info("Database connection closed")
+
+
+def add_application_to_db(job_id, application):
+    try:
+        db_conn = connect_with_db()
+        cursor = db_conn.cursor(dictionary=True)
+        query = """INSERT INTO applications 
+            (job_id, full_name, email, linkedin_url, education, work_experience)
+            VALUES (%s, %s, %s, %s, %s, %s)"""
+        cursor.execute(
+            query, 
+            (job_id, 
+            application['full_name'], 
+            application['email'], 
+            application['linkedin_url'],
+            application['education'],
+            application['work_experience'])
+            )
+        db_conn.commit()
+        return True
+    except Exception as e:
+        logging.error(f"Failed to insert or update application into the database: {e}")
+        return False
+    finally:
+        if db_conn.is_connected():
+            cursor.close()
+            db_conn.close()
+            logging.info("Database connection closed")
+        
+
+    
 
 
